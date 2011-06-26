6962e155874df1d206f31f3ff5baf7678eac8048
David Petrovics <dpetrovics@gmail.com>
2011-06-26 07:43:07 -0400

    c1, c2: added store controller, updated the std page layout, linked it to our css, new index page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'   #will return product lists in alphabetical order
+  
+  #validation stuff
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
