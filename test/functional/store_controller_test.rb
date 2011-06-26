e26cf250925f05bd9b472a9443b90e743cdab9e3
David Petrovics <dpetrovics@gmail.com>
2011-06-26 07:59:42 -0400

    c3,c4: added a helper to format prices, wrote functional tests for the store controller

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', :minimum => 4   #looks for element named a, contained in elem w id side, contained in elem w id columns. (our 4 links)
+    assert_select '#main .entry', 3   #should be 3 entries from our test db (fixtures) 
+    assert_select 'h3', 'Programming Ruby 1.9' #should be a title with that name
+    assert_select '.price', /\$[,\d]+\.\d\d/  #reg expr: $ followed by at least 1 commas or digits, then '.', then 2 digits
   end
 
 end
