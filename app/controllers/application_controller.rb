d76422d9eddec5585e4fb89b1f36bbb2f9a4ec58
David Petrovics <dpetrovics@gmail.com>
2011-06-26 21:42:02 -0400

    created cart model, defined current_cart method in the app ctrlr-- fetches the cart corresponding to cart_id from the session. created line_item model, with properties product_id and cart_id. Defined interrelationships (has_many, belongs_to).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private   #means the methods below are only available to other cntrlrs, it is not availble as
+            # an action on the controller
+  
+    def current_cart
+      Cart.find(session[:cart_id])   #get the :cart_id from session, and return corresponding cart
+    rescue ActiveRecord::RecordNotFound   #if the cart_id isnt found or is nil, create a new cart
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
 end
