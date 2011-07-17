643d28a63723f3693e0d83402e74402d0a86a23e
David Petrovics <dpetrovics@gmail.com>
2011-07-17 14:18:48 -0500

    G3: modified gemfile to add the will_paginate gem (plugin). Created load_orders.rb script which creates 100 orders. Edited the orderscontroller and orders index view to use the paginate plugin.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,11 @@
 class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.xml
-  def index
-    @orders = Order.all
+  def index  
+    @orders = Order.paginate :page=>params[:page], :order=>'created_at desc',
+      :per_page => 10
+    
+    # @orders = Order.all
 
     respond_to do |format|
       format.html # index.html.erb
---
+++ script/load_orders.rb
@@ -0,0 +1,8 @@
+#creates 100 orders with no line items, 
+
+Order.transaction do 
+  (1..100).each do |i|
+      Order.create(:name => "Customer #{i}", :address => "#{i} Main Street", 
+      :email => "customer-#{i}@example.com", :pay_type => "Check")
+  end 
+end
